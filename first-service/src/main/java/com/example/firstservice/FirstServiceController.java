57ba04fa805e9ec01694c4cc1ba36df2f5e74e5e
조현덕 <gusejr4547@gmail.com>
2024-12-05 17:19:00 +0900

    init: spring cloud gateway 사용해서 서비스 2개 라우팅하는 예제

---
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,16 @@
+package com.example.firstservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// http://localhost:8081/welcome
+// http://localhost:8081/first-service/welcome
+@RestController
+@RequestMapping("/first-service")
+public class FirstServiceController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to the First Service";
+    }
+}
